d9e420bfcbdbfcbf0bf5536aa29a97d147b548d3
Sultan Khan <sultan.mobilefirst@gmail.com>
2023-05-03 17:53:39 +0530

    test app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/home', (req, res) => {
+  return res.status(200).send('Hello World!');
 });
 
 
